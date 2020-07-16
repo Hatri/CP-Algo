a03f9f8848c6aab8fceb68663713e84c3b8f1aa3
Hatri <18404171+Hatri@users.noreply.github.com>
2020-07-16 19:57:01 +0200

    I/O Testcases

---
+++ IO.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    freopen("IO_in1.txt", "r", stdin);
+    int Testcases;
+    scanf("%d", &Testcases); // Number of test cases
+}
