6c3cfe2748e5957f489a3647aed2a7120e9d24cc
Hatri <18404171+Hatri@users.noreply.github.com>
2020-07-16 19:59:38 +0200

    Update IO.cpp

--- IO.cpp
+++ IO.cpp
@@ -7,4 +7,12 @@ int main()
     freopen("IO_in1.txt", "r", stdin);
     int Testcases;
     scanf("%d", &Testcases); // Number of test cases
+    while (Testcases--)
+    { // Repeat until 0
+        int a, b;
+        scanf("%d %d", &a, &b);
+        printf("%d\n", a + b);
+    }
+
+    return 0;
 }
