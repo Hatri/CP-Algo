65e4209cba7e22f4622d81a442acf1de106d368c
Hatri <18404171+Hatri@users.noreply.github.com>
2020-07-19 01:20:52 +0200

    Setup
    added Vowel

---
+++ basicString.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int isvowel(char ch)
+{
+    char vowel[6] = "aeiou";
+    for (int j = 0; vowel[j]; j++)
+        if (vowel[j] == ch)
+            return 1;
+    return 0;
+}
