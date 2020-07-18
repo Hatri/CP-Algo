cf0d9bdadb9f222b1431fe23346999ec957a1594
Hatri <18404171+Hatri@users.noreply.github.com>
2020-07-18 21:05:20 +0200

    Update moveRomanNum.cpp
    Added roman to arabic

--- moveRomanNum.cpp
+++ moveRomanNum.cpp
@@ -3,17 +3,17 @@ using namespace std;
 
 typedef pair<int, string> is;
 
-void ArabicToRoman(int answer)
+void ArabicToRoman(int arabic)
 {
     vector<is> convert({{1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"}, {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"}, {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}
 
     });
 
     for (auto &[value, roman] : convert) // Going from large to small
-        while (answer >= value)
+        while (arabic >= value)
         {
             cout << roman;
-            answer -= value;
+            arabic -= value;
         }
 
     cout << "\n";
@@ -39,4 +39,18 @@ int value(char letter)
         return 1000;
     }
     return 0;
+}
+
+void RomanToArabic(string roman)
+{
+    int answer = 0;
+    for (int i = 0; roman[i]; ++i)
+        if (roman[i + 1] && (value(roman[i]) < value(roman[i + 1])))
+        {                                                    // Check next char first
+            answer += value(roman[i + 1]) - value(roman[i]); // by defintion
+            ++i;                                             // skip this char
+        }
+        else
+            ans += value(roman[i]);
+    cout << ans << "\n";
 }
