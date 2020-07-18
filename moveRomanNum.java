e66735791e186e91e97270d0a692debbb75b5758
Hatri <18404171+Hatri@users.noreply.github.com>
2020-07-18 21:11:34 +0200

    Setup

---
+++ moveRomanNum.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    private static PrinterWriter pw;
+
+    private static void ArabicToRoman(int arabic) {
+        // process from larger value to smaller value
+        TreeMap<Integer, String> cvt = new TreeMap<Integer, String>(Collections.reverseOrder());
+        cvt.put(1000, "M");
+        cvt.put(900, "CM");
+        cvt.put(500, "D");
+        cvt.put(400, "CD");
+        cvt.put(100, "C");
+        cvt.put(90, "XC");
+        cvt.put(50, "L");
+        cvt.put(40, "XL");
+        cvt.put(10, "X");
+        cvt.put(9, "IX");
+        cvt.put(5, "V");
+        cvt.put(4, "IV");
+        cvt.put(1, "I");
+    }
+}
