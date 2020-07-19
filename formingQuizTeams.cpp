84821a8d44e5c08bf5068ba1f6a45a55f6ad2acd
Hatri <18404171+Hatri@users.noreply.github.com>
2020-07-19 13:52:40 +0200

    Update formingQuizTeams.cpp

--- formingQuizTeams.cpp
+++ formingQuizTeams.cpp
@@ -6,3 +6,11 @@ using namespace std;
 
 int N;
 double dist[20][20], memo[1 << 16]; // 1 << 16 is 2^16, recall that max N = 8
+
+double matching(int bit_mask)
+{
+    if (memo[bit_mask] > -0.5) // see that we initialize the array with -1 in the main function
+        return memo[bit_mask];
+    if (bit_mask == (1 << 2 * N) - 1) // all are matched
+        return memo[bit_mask] = 0;
+}
