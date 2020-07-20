107ab7770d11ffcaa1f24c0ab1c296698e741d46
Hatri <18404171+Hatri@users.noreply.github.com>
2020-07-20 20:06:53 +0200

    Update basicString.cpp
    While funct

--- basicString.cpp
+++ basicString.cpp
@@ -21,4 +21,17 @@ int main()
     strcpy(str, "");
     bool first = true;                         // technique to differentiate first line with the other lines
     bool prev_dash = false, this_dash = false; // to differentiate whether the previous line contains a dash or not
+    while (1)
+    {
+        char line[110];
+        fgets(line, 100, stdin);
+        line[(int)strlen(line) - 1] = 0; // deletes dummy char
+        if (strncmp(line, ".......", 7) == 0)
+            break;
+        if (line[(int)strlen(line) - 1] == '-')
+        {
+            line[(int)strlen(line) - 1] = 0; // if the last character is '-', deletes it by moving the null (0) one character forward
+            this_dash = true;
+        }
+    }
 }
