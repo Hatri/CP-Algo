3d8deadc53c8f4834ffc8e15e27f75977b11983a
Hatri <18404171+Hatri@users.noreply.github.com>
2020-07-20 20:04:10 +0200

    Update basicString.cpp

--- basicString.cpp
+++ basicString.cpp
@@ -10,3 +10,15 @@ int isvowel(char ch)
             return 1;
     return 0;
 }
+
+int main()
+{
+    int pos, digits, alphas, vowels, consonants;
+    char str[10010], *p;
+
+    freopen("basic_string_in.txt", "r", stdin);
+
+    strcpy(str, "");
+    bool first = true;                         // technique to differentiate first line with the other lines
+    bool prev_dash = false, this_dash = false; // to differentiate whether the previous line contains a dash or not
+}
