42bc5131b26078cd0b096df7362fba6ea1ab92ed
Hatri <18404171+Hatri@users.noreply.github.com>
2020-08-07 17:47:06 +0200

    Update

--- UVA.java
+++ UVA.java
@@ -1,10 +1,15 @@
+import jdk.internal.org.jline.utils.InputStreamReader;
 import jdk.internal.util.StaticProperty;
 import java.io.*;
 import java.util.*;
 import java.math.BigInteger;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out))); // = fast IO
+
+        int caseNo = 0;
 
     }
 
