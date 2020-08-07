017a591856b2267726e1a4908d567b2748c3b359
Hatri <18404171+Hatri@users.noreply.github.com>
2020-08-07 17:44:10 +0200

    Setup

---
+++ UVA.java
@@ -0,0 +1,11 @@
+import jdk.internal.util.StaticProperty;
+import java.io.*;
+import java.util.*;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+}
