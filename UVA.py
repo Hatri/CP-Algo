cfe13aa8c47f65dc518d4acd1f495a39794ba750
Hatri <18404171+Hatri@users.noreply.github.com>
2020-08-09 22:34:36 +0200

    Update

--- UVA.py
+++ UVA.py
@@ -2,3 +2,10 @@ import sys
 inputs = sys.stdin.read().splitlines()
 caseNo = 1
 ln = 0
+
+while True:
+    Bills, Friends = map(int, inputs[ln].split())
+    ln += 1
+
+    if Bills == 0 and Friends == 0:
+        break
