df27614ce89fe73711383931beb22a3f5001275d
Hatri <18404171+Hatri@users.noreply.github.com>
2020-08-15 19:08:22 +0200

    Update arrayAlgorithms.cpp

--- arrayAlgorithms.cpp
+++ arrayAlgorithms.cpp
@@ -18,3 +18,34 @@ bool icpcCmp(team a, team b)
     else
         return a.id < b.id;
 }
+
+int main()
+{
+    int arr[] = {10, 7, 2, 15, 4};
+    vector<int> v(arr, arr + 5); // alternative constructor
+
+    // sort descending with vector
+    sort(v.rbegin(), v.rend()); // reverse iterator
+    for (auto &val : v)
+        printf("%d ", val); // access the values
+    printf("\n");
+    printf("==================\n");
+
+    // sort descending with integer array
+    sort(arr, arr + 5);    // ascending
+    reverse(arr, arr + 5); // then reverse
+    for (int i = 0; i < 5; ++i)
+        printf("%d ", arr[i]);
+    printf("\n");
+    printf("==================\n");
+
+    shuffle(v.begin(), v.end(), default_random_engine()); // shuffle the content
+    for (auto &val : v)
+        printf("%d ", val);
+    printf("\n");
+    printf("==================\n");
+    partial_sort(v.begin(), v.begin() + 2, v.end()); // partial_sort demo
+    for (auto &val : v)
+        printf("%d ", val);
+    printf("\n");
+    printf("==================\n");
